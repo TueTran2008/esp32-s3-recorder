c638f27df5e305b952aa4c79d00210d25b20a23c
TueTran2008 <tuetd174343@gmail.com>
2025-05-24 10:52:28 +0700

    ble stream but hear bad

--- main/streamer.c
+++ main/streamer.c
@@ -32,9 +32,9 @@
 #include "http_stream.h"
 #include "audio_mem.h"
 #include "audio_thread.h"
-#include "media_lib_adapter.h"
-#include "audio_idf_version.h"
-#include "tcp_client_stream.h"
+#include "esp_peripherals.h"
+#include "esp_gap_bt_api.h"
+#include "a2dp_stream.h"
 
 #if (ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 1, 0))
 #include "esp_netif.h"
@@ -50,11 +50,153 @@
 
 extern audio_hal_func_t AUDIO_CODEC_ES8388_DEFAULT_HANDLE;
 
-static const char *TAG = "TCP Streamer";
+
 static bool board_init = false;
 
 static audio_pipeline_handle_t pipeline;
-static audio_element_handle_t i2s_stream_reader, tcp_stream_writer;
+static audio_element_handle_t i2s_stream_reader, bt_stream_writer;
+
+
+typedef uint8_t esp_peer_bdname_t[ESP_BT_GAP_MAX_BDNAME_LEN + 1];
+
+static const char *TAG = "BLE_SOURCE_WRITER";
+static esp_peer_bdname_t remote_bt_device_name;
+static bool device_found = false;
+static esp_bd_addr_t remote_bd_addr = {0};
+
+static char *bda2str(esp_bd_addr_t bda, char *str, size_t size)
+{
+    if (bda == NULL || str == NULL || size < 18) {
+        return NULL;
+    }
+
+    uint8_t *p = bda;
+    sprintf(str, "%02x:%02x:%02x:%02x:%02x:%02x",
+            p[0], p[1], p[2], p[3], p[4], p[5]);
+    return str;
+}
+
+static bool get_name_from_eir(uint8_t *eir, uint8_t *bdname, uint8_t *bdname_len)
+{
+    uint8_t *rmt_bdname = NULL;
+    uint8_t rmt_bdname_len = 0;
+
+    if (!eir) {
+        return false;
+    }
+
+    rmt_bdname = esp_bt_gap_resolve_eir_data(eir, ESP_BT_EIR_TYPE_CMPL_LOCAL_NAME, &rmt_bdname_len);
+    if (!rmt_bdname) {
+        rmt_bdname = esp_bt_gap_resolve_eir_data(eir, ESP_BT_EIR_TYPE_SHORT_LOCAL_NAME, &rmt_bdname_len);
+    }
+
+    if (rmt_bdname) {
+        if (rmt_bdname_len > ESP_BT_GAP_MAX_BDNAME_LEN) {
+            rmt_bdname_len = ESP_BT_GAP_MAX_BDNAME_LEN;
+        }
+
+        if (bdname) {
+            memcpy(bdname, rmt_bdname, rmt_bdname_len);
+            bdname[rmt_bdname_len] = '\0';
+        }
+        if (bdname_len) {
+            *bdname_len = rmt_bdname_len;
+        }
+        return true;
+    }
+
+    return false;
+}
+
+static void filter_inquiry_scan_result(esp_bt_gap_cb_param_t *param)
+{
+    char bda_str[18];
+    uint32_t cod = 0;
+    int32_t rssi = -129; /* invalid value */
+    uint8_t *eir = NULL;
+    esp_peer_bdname_t peer_bdname;
+    esp_bt_gap_dev_prop_t *p;
+
+    ESP_LOGI(TAG, "Scanned device: %s", bda2str(param->disc_res.bda, bda_str, 18));
+    for (int i = 0; i < param->disc_res.num_prop; i++) {
+        p = param->disc_res.prop + i;
+        switch (p->type) {
+            case ESP_BT_GAP_DEV_PROP_COD:
+                cod = *(uint32_t *)(p->val);
+                ESP_LOGI(TAG, "--Class of Device: 0x%" PRIx32, cod);
+                break;
+            case ESP_BT_GAP_DEV_PROP_RSSI:
+                rssi = *(int8_t *)(p->val);
+                ESP_LOGI(TAG, "--RSSI: %" PRId32, rssi);
+                break;
+            case ESP_BT_GAP_DEV_PROP_EIR:
+                eir = (uint8_t *)(p->val);
+                get_name_from_eir(eir, (uint8_t *)&peer_bdname, NULL);
+                ESP_LOGI(TAG, "--Name: %s", peer_bdname);
+                break;
+            case ESP_BT_GAP_DEV_PROP_BDNAME:
+            default:
+                break;
+        }
+    }
+
+    ESP_LOGI(TAG, "need device name %s", (uint8_t *)remote_bt_device_name);
+    /* search for device named "peer_bdname" in its extended inquiry response */
+    if (eir) {
+        get_name_from_eir(eir, (uint8_t *)&peer_bdname, NULL);
+        if (strcmp((char *)peer_bdname, (char *)remote_bt_device_name) != 0) {
+            return;
+        }
+
+        ESP_LOGI(TAG, "Found a target device, address %s, name %s", bda_str, (uint8_t *)peer_bdname);
+        device_found = true;
+        memcpy(&remote_bd_addr, param->disc_res.bda, ESP_BD_ADDR_LEN);
+        ESP_LOGI(TAG, "Cancel device discovery ...");
+        esp_bt_gap_cancel_discovery();
+    }
+}
+
+static void bt_app_gap_cb(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
+{
+    switch (event) {
+        case ESP_BT_GAP_DISC_RES_EVT: {
+                filter_inquiry_scan_result(param);
+                break;
+            }
+        case ESP_BT_GAP_DISC_STATE_CHANGED_EVT: {
+                if (param->disc_st_chg.state == ESP_BT_GAP_DISCOVERY_STOPPED) {
+                    if (device_found) {
+                        ESP_LOGI(TAG, "Device discovery stopped.");
+                        ESP_LOGI(TAG, "a2dp connecting to peer: %s", remote_bt_device_name);
+                        device_found = false;
+                        esp_a2d_source_connect(remote_bd_addr);
+                    } else {
+                        // not discovered, continue to discover
+                        ESP_LOGI(TAG, "Device discovery failed, continue to discover...");
+                        esp_bt_gap_start_discovery(ESP_BT_INQ_MODE_GENERAL_INQUIRY, 10, 0);
+                    }
+                } else if (param->disc_st_chg.state == ESP_BT_GAP_DISCOVERY_STARTED) {
+                    ESP_LOGI(TAG, "Discovery started.");
+                }
+                break;
+            }
+        case ESP_BT_GAP_PIN_REQ_EVT: {
+                ESP_LOGI(TAG, "ESP_BT_GAP_PIN_REQ_EVT min_16_digit:%d", param->pin_req.min_16_digit);
+                if (param->pin_req.min_16_digit) {
+                    ESP_LOGI(TAG, "Input pin code: 0000 0000 0000 0000");
+                    esp_bt_pin_code_t pin_code = {0};
+                    esp_bt_gap_pin_reply(param->pin_req.bda, true, 16, pin_code);
+                } else {
+                    esp_bt_pin_code_t pin_code = {'1', '2', '3', '4'};
+                    esp_bt_gap_pin_reply(param->pin_req.bda, true, 4, pin_code);
+                }
+                break;
+            }
+        default:
+            break;
+    }
+    return;
+}
 
 static void log_init(void) {
     esp_log_level_set("*", ESP_LOG_DEBUG);
@@ -65,17 +207,9 @@ static void log_init(void) {
     esp_log_level_set("OPUS_ENCODER", ESP_LOG_VERBOSE);
 }
 
-static void ip_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
-    if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        // esp_event_post_to(event_loop_handle, BOARD_EVENT_BASE, BOARD_EVENT_RECORD, NULL, 0,
-        //               portMAX_DELAY)
-        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
-        ESP_LOGE(TAG, "ESP got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-    }
-}
-
 void app_main() {
     esp_periph_config_t periph_cfg = DEFAULT_ESP_PERIPH_SET_CONFIG();
+    esp_periph_set_handle_t set = esp_periph_set_init(&periph_cfg);
     int sample_rate = 0;
     esp_err_t err = nvs_flash_init();
     if (err == ESP_ERR_NVS_NO_FREE_PAGES) {
@@ -92,23 +226,6 @@ void app_main() {
 
     log_init();
 
-
-    ESP_LOGI(TAG, "[1.0] Wifi Connection");
-    esp_periph_set_handle_t set = esp_periph_set_init(&periph_cfg);
-
-    periph_wifi_cfg_t wifi_cfg = {
-        .wifi_config.sta.ssid = CONFIG_WIFI_SSID,
-        .wifi_config.sta.password = CONFIG_WIFI_PASSWORD,
-    };
-
-    esp_periph_handle_t wifi_handle = periph_wifi_init(&wifi_cfg);
-
-    // Start wifi & button peripheral
-    esp_periph_start(set, wifi_handle);
-    esp_event_handler_instance_t instance_got_ip;
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &ip_event_handler, NULL, &instance_got_ip));
-
-    periph_wifi_wait_for_connected(wifi_handle, portMAX_DELAY);
 
     ESP_LOGI(TAG, "[2.0] Create audio pipeline for recording");
     audio_pipeline_cfg_t pipeline_cfg = DEFAULT_AUDIO_PIPELINE_CONFIG();
@@ -137,29 +254,63 @@ void app_main() {
     i2s_stream_reader = i2s_stream_init(&i2s_cfg);
 
     /////////////////
-    ESP_LOGI(TAG, "[3.1] Create tcp client stream to write data");
-    tcp_stream_cfg_t tcp_cfg = TCP_STREAM_CFG_DEFAULT();
-    tcp_cfg.type = AUDIO_STREAM_WRITER;
-    tcp_cfg.port = CONFIG_TCP_PORT;
-    tcp_cfg.host = CONFIG_TCP_URL;
-    tcp_cfg.ext_stack = false;
-    tcp_stream_writer = tcp_stream_init(&tcp_cfg);
-    AUDIO_NULL_CHECK(TAG, tcp_stream_writer, return);
+    ESP_LOGI(TAG, "[3.1] Create Bluetooth stream");
+
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    esp_bt_pin_type_t pin_type = ESP_BT_PIN_TYPE_FIXED;
+    esp_bt_pin_code_t pin_code = {'1', '2', '3', '4'};
+    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_BLE));
+    ESP_ERROR_CHECK(esp_bt_controller_init(&bt_cfg));
+    ESP_ERROR_CHECK(esp_bt_controller_enable(ESP_BT_MODE_CLASSIC_BT));
+    ESP_ERROR_CHECK(esp_bluedroid_init());
+    ESP_ERROR_CHECK(esp_bluedroid_enable());
+
+    esp_bt_gap_set_device_name("ESP_SOURCE_STREAM_DEMO");
+    esp_bt_gap_set_pin(pin_type, 4, pin_code);
+    esp_bt_gap_register_callback(bt_app_gap_cb);
+
+    const char *remote_name = NULL;
+    remote_name = CONFIG_BT_REMOTE_NAME;
+    if (remote_name) {
+        memcpy(&remote_bt_device_name, remote_name, strlen(remote_name) + 1);
+    } else {
+        memcpy(&remote_bt_device_name, "ESP_SINK_STREAM_DEMO", ESP_BT_GAP_MAX_BDNAME_LEN);
+    }
+
+    a2dp_stream_config_t a2dp_config = {
+        .type = AUDIO_STREAM_WRITER,
+        .user_callback = {0},
+    };
+    bt_stream_writer = a2dp_stream_init(&a2dp_config);
+
+    esp_bt_gap_start_discovery(ESP_BT_INQ_MODE_GENERAL_INQUIRY, 10, 0);
+#if (ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 0, 0))
+    esp_bt_gap_set_scan_mode(ESP_BT_CONNECTABLE, ESP_BT_GENERAL_DISCOVERABLE);
+#else
+    esp_bt_gap_set_scan_mode(ESP_BT_SCAN_MODE_CONNECTABLE_DISCOVERABLE);
+#endif
 
     audio_element_info_t info = AUDIO_ELEMENT_INFO_DEFAULT();
     audio_element_getinfo(i2s_stream_reader, &info);
 
-    audio_pipeline_register(pipeline, i2s_stream_reader, "i2s");
-    audio_pipeline_register(pipeline, tcp_stream_writer, "tcp");
+    ESP_LOGI(TAG, "[3.3] Register all elements to audio pipeline");
 
+    audio_pipeline_register(pipeline, bt_stream_writer, "bt");
+    audio_pipeline_register(pipeline, i2s_stream_reader, "i2s");
     ESP_LOGI(TAG, "[3.2] Link it together "
                   "[codec_chip]-->i2s_stream-->tcp_stream_writer");
     // const char *link_wav[3] = {"i2s", "wav", "wav_file"};
-    const char *link[2] = {"i2s", "tcp"};
+    const char *link[2] = {"i2s", "bt"};
 
     audio_pipeline_link(pipeline, &link[0], 2);
 
+    ESP_LOGI(TAG, "[3.3] Create bt peripheral");
+    esp_periph_handle_t bt_periph = bt_create_periph();
 
+    ESP_LOGI(TAG, "[3.4] Start bt peripheral");
+    esp_periph_start(set, bt_periph);
+/////////////////////////////////////////////// Config Listener
+    ESP_LOGI(TAG, "[ 4 ] Set up  event listener");
     audio_event_iface_cfg_t evt_cfg = AUDIO_EVENT_IFACE_DEFAULT_CFG();
     audio_event_iface_handle_t evt = audio_event_iface_init(&evt_cfg);
 
@@ -170,27 +321,60 @@ void app_main() {
     audio_event_iface_set_listener(esp_periph_set_get_event_iface(set), evt);
 
     ESP_LOGI(TAG, "[ 5 ] Start audio_pipeline");
+
+    ESP_LOGI(TAG, "[ 5 ] Start audio_pipeline");
     audio_pipeline_run(pipeline);
     while (1) {
-        audio_event_iface_msg_t msg;
-        esp_err_t ret = audio_event_iface_listen(evt, &msg, portMAX_DELAY);
-        if (ret != ESP_OK) {
-            ESP_LOGE(TAG, "[ * ] Event interface error : %d", ret);
-            continue;
-        }
-        ESP_LOGI(TAG, "[ * ] Event received:");
-        ESP_LOGI(TAG, "    Source Type: %d", msg.source_type);
-        ESP_LOGI(TAG, "    Command: %d", msg.cmd);
-        ESP_LOGI(TAG, "    Source Handle: %p", msg.source);
-        ESP_LOGI(TAG, "    Data: %p", msg.data);
-        ESP_LOGI(TAG, "    Data Length: %d", msg.data_len);
+		audio_event_iface_msg_t msg;
+	    esp_err_t ret = audio_event_iface_listen(evt, &msg, portMAX_DELAY);
+	    if (ret != ESP_OK) {
+	        ESP_LOGE(TAG, "[ * ] Event interface error : %d", ret);
+	        continue;
+	    }
 
-        if (msg.source_type == AUDIO_ELEMENT_TYPE_ELEMENT && msg.source == (void *) tcp_stream_writer
-            && msg.cmd == AEL_MSG_CMD_REPORT_STATUS
-            && (((int)msg.data == AEL_STATUS_STATE_STOPPED) || ((int)msg.data == AEL_STATUS_ERROR_OUTPUT) || ((int)msg.data == AEL_STATUS_ERROR_OPEN)  || ((int)msg.data == AEL_STATUS_ERROR_INPUT) || ((int)msg.data == AEL_STATUS_ERROR_PROCESS) || ((int)msg.data == AEL_STATUS_ERROR_TIMEOUT) || ((int)msg.data == AEL_STATUS_ERROR_CLOSE) || ((int)msg.data == AEL_STATUS_ERROR_UNKNOWN)  )) {
-            ESP_LOGE(TAG, "TCP Stop :%d", (int)msg.data);
+	    // if (msg.source_type == AUDIO_ELEMENT_TYPE_ELEMENT
+	    //     && msg.source == (void *) mp3_decoder
+	    //     && msg.cmd == AEL_MSG_CMD_REPORT_MUSIC_INFO) {
+	    //     audio_element_info_t music_info = {0};
+	    //     audio_element_getinfo(mp3_decoder, &music_info);
 
-
-        }
+	    //     ESP_LOGI(TAG, "[ * ] Receive music info from mp3 decoder, sample_rates=%d, bits=%d, ch=%d",
+	    //              music_info.sample_rates, music_info.bits, music_info.channels);
+	    //     continue;
+	    // }
+	    if (msg.source_type == PERIPH_ID_BLUETOOTH
+	        && msg.source == (void *)bt_periph) {
+	        if ((msg.cmd == PERIPH_BLUETOOTH_DISCONNECTED) || (msg.cmd == PERIPH_BLUETOOTH_AUDIO_SUSPENDED)) {
+	            ESP_LOGW(TAG, "[ * ] Bluetooth disconnected or suspended");
+	            periph_bt_stop(bt_periph);
+	            break;
+	        }
+	    }
     }
+    ESP_LOGI(TAG, "[ 7 ] Stop audio_pipeline");
+    audio_pipeline_stop(pipeline);
+    audio_pipeline_wait_for_stop(pipeline);
+    audio_pipeline_terminate(pipeline);
+
+    /* Terminal the pipeline before removing the listener */
+    audio_pipeline_remove_listener(pipeline);
+
+    /* Stop all periph before removing the listener */
+    esp_periph_set_stop_all(set);
+    audio_event_iface_remove_listener(esp_periph_set_get_event_iface(set), evt);
+
+    /* Make sure audio_pipeline_remove_listener & audio_event_iface_remove_listener are called before destroying event_iface */
+    audio_event_iface_destroy(evt);
+
+    /* Release all resources */
+    audio_pipeline_unregister(pipeline, bt_stream_writer);
+    audio_pipeline_unregister(pipeline, i2s_stream_reader);
+    audio_pipeline_deinit(pipeline);
+    audio_element_deinit(bt_stream_writer);
+    esp_periph_set_destroy(set);
+    esp_bluedroid_disable();
+    esp_bluedroid_deinit();
+    esp_bt_controller_disable();
+    esp_bt_controller_deinit();
+    esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT);
 }
